3fba555946e7d37c5ac4b9e6b19fe30d1cbbf742
DelayVeshi <derevchuk.s@gmail.com>
2024-01-25 20:00:28 +0300

    Добавление во view.py главных страниц

--- main/views.py
+++ main/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 
 
-def index(request):
+def index(request): # Передать в context нужно только что-то одно
     context = {
-        'title': 'Hair salon'
+        'home': 'Hair salon - Home',
+        'women': 'Hair salon - Women',
+        'men': 'Hair salon - Men',
+        'junior': 'Hair salon - Junior'
+
     }
     return render(request, 'index.html', context)
+
+
+def index2(request): # Передать в context нужно только что-то одно
+    context = {
+        'home': 'Hair salon - Home',
+        'women': 'Hair salon - Women',
+        'men': 'Hair salon - Men',
+        'junior': 'Hair salon - Junior'
+
+    }
+    return render(request, 'index2.html', context)
+
+
+def homepage3(request): # Передать в context нужно только что-то одно
+    context = {
+        'home': 'Hair salon - Home',
+        'women': 'Hair salon - Women',
+        'men': 'Hair salon - Men',
+        'junior': 'Hair salon - Junior'
+
+    }
+    return render(request, 'homepage-3.html', context)
 

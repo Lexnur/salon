80e7c452d99aa5669bc9869716d4aec0b7698dac
DelayVeshi <derevchuk.s@gmail.com>
2024-01-28 14:26:01 +0300

    Начало работы со страницей men.html

--- main/views.py
+++ main/views.py
@@ -18,13 +18,10 @@ def index(request):
 
 def index2(request): # Передать в context нужно только что-то одно
     context = {
-        'home': 'Hair salon - Home',
-        'women': 'Hair salon - Women',
-        'men': 'Hair salon - Men',
-        'junior': 'Hair salon - Junior'
+        'men': 'Hair salon - For men',
 
     }
-    return render(request, 'index2.html', context)
+    return render(request, 'men.html', context)
 
 
 def homepage3(request): # Передать в context нужно только что-то одно

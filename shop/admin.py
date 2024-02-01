9c8d2d1f19088c237aa14482aa5f25e465f9a1c7
DelayVeshi <derevchuk.s@gmail.com>
2024-02-01 19:09:10 +0300

    Добавление models and admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from shop.models import Categories, Products
 
-# Register your models here.
+
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+

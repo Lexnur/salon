b5638bc717c71c84b1f167b06f92d87157944bde
DelayVeshi <derevchuk.s@gmail.com>
2024-02-10 12:09:06 +0300

    Подключение БД - PostgreSQL. Каталог и товары надо заново забивать.

--- salon/settings.py
+++ salon/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'salon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'salon',
+        'USER': 'salon',
+        'PASSWORD': 'salon',
+        'HOST': 'localhost',
+        'Port': '5432',
     }
 }
+
 
 
 # Password validation

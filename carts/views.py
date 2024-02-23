788d852e519c0b0eb6725014b486d5e02df9bf5e
DelayVeshi <derevchuk.s@gmail.com>
2024-02-23 17:44:35 +0300

    Сделал удаление товаров из корзины

--- carts/views.py
+++ carts/views.py
@@ -18,8 +18,10 @@ def cart_add(request, product_slug):
     return redirect(request.META['HTTP_REFERER'])
 
 
-def cart_remove(request, product_slug):
-    return render(request, '')
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META['HTTP_REFERER'])
 
 
 def cart_change(request, product_slug):
